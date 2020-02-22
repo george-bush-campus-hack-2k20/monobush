7aed5dd2db2c7ae4b15fe02ab1fcaa343d3d8a20
Dan Goje <gojeda98@gmail.com>
2020-02-22 15:51:52 +0000

    Add mock trap implementation

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,45 @@ import React from "react";
 import "./App.css";
 
 import { Spinner } from "./pages/Spinner";
+import { Trap } from "./pages/Trap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"><Spinner /></header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    clientStage: "waiting",
+    trap: null
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        clientStage: "playing",
+        trap: {
+          type: "level",
+          color: "#ffffff",
+          text: "Text"
+        }
+      });
+    }, 5000);
+  };
+
+  renderPage = () => {
+    switch (this.state.clientStage) {
+      case "waiting":
+        return <Spinner />;
+      case "playing":
+        return <Trap trapData={this.state.trap} />;
+      default:
+        console.err("Something went terribly wrong...");
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">{this.renderPage()}</header>
+      </div>
+    );
+  }
 }
 
 export default App;
