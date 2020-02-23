73f2913872fba1a9e8d862e3ee2cdb0a01c3b8ea
Kajetan Champlewski <kajetan@kch.dev>
2020-02-23 08:35:47 +0000

    pee pee poo poo

--- building_seven/test/destroy_trap.py
+++ building_seven/test/destroy_trap.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.post("http://127.0.0.1:3000/game/destroy_trap", json={"id": "4229965d-6cd1-411f-ba8f-33cdf0522c21"})
+r = requests.post("http://127.0.0.1:8080/game/destroy_trap", json={"id": "4229965d-6cd1-411f-ba8f-33cdf0522c21"})
 print(r.url)
 print(r.content)
 print(r.status_code)
